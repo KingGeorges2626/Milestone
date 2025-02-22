072d1043f6bd6f321e7d301a6702e70f33c358ba
KingGeorges2626 <milestoneapp@proton.me>
2025-02-22 01:34:23 +0100

    Delete src directory

--- src/pages/ChallengeDetail.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const ChallengeDetail = () => {
-  return <h1>Détail du Défi</h1>;
-};
-
-export default ChallengeDetail;
--- src/pages/Challenges.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Challenges = () => {
-  return <h1>Liste des Défis</h1>;
-};
-
-export default Challenges;
