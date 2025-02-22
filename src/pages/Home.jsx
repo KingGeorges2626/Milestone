7442f9039cb4d2601bc023d6f3260f9705b42d91
KingGeorges2626 <milestoneapp@proton.me>
2025-02-22 01:43:21 +0100

    Add files via upload

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Bienvenue sur Milestone</h1>
+      <p>La plateforme pour relever des défis et progresser.</p>
+    </div>
+  );
+};
+
+export default Home;
